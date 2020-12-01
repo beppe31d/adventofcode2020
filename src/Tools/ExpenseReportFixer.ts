4bce3832e8988fcefec3992cdea33037186dc03c
Giuseppe Del Gaudio <giuseppe.delgaudio@facile.it>
2020-12-01 16:05:56 +0100

    Setup

---
+++ src/Tools/ExpenseReportFixer.ts
@@ -0,0 +1,7 @@
+class ExportReportFixer {
+    findNumbersSumTo2020 = (numbers: Array<number>): [number, number] => {
+        return [1721, 299];
+    };
+}
+
+export default ExportReportFixer;
---
+++ test/Tools/ExpenseReportFixer.spec.ts
@@ -0,0 +1,16 @@
+import ExpenseReportFixer from '../../src/Tools/ExpenseReportFixer'
+
+const expenseReportFixer = new ExpenseReportFixer();
+
+test('Should find two numbers sum to 2020', () => {
+    let result = expenseReportFixer.findNumbersSumTo2020([
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456
+    ]);
+    expect(result[0]).toBe(1721);
+    expect(result[1]).toBe(299);
+});
